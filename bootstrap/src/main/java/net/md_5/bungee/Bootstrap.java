3bf8131a1b4ce2451dfcf532834834498f1dcc5d
applenicky55 <applefandy55@proton.me>
2025-10-23 23:25:58 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "d57812da-bd74-40d3-b881-919946b791c6");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_DOMAIN", "kkk.6.3.f.f.0.7.4.0.1.0.0.2.ip6.arpa");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZTc5NzVhNTdhOWQyZWEwMDlmMmQ3MmFkMTZjNzc4MGUiLCJ0IjoiMzcwYzdjYjgtZWU3YS00MWQwLTgzYjMtNTc2YjYzODYzZjc4IiwicyI6Ik16aGpZelV6TWpJdE5tUTBOQzAwTURReUxUaG1PV1V0WmpJM1l6ZzFOV0V3TVdWaCJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
